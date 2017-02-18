7b9b8764f5e2488a61468009853a737fad6fc77e
dzdaniel84 <dzdaniel84@gmail.com>
2017-02-18 14:33:35 -0800

    Fixed function

--- watson.py
+++ watson.py
@@ -9,8 +9,8 @@ discovery = DiscoveryV1(
   version="2016-12-01"
 )
 
-environment_id = 'f8c120ac-c67a-4983-ba71-693704418700'
-collection_id = '33821e0a-5fef-4675-9f98-478b6f49cbe7'
+environment_id = '59401ea6-3614-4c75-baae-90231dcadd6b'
+collection_id = '776be5b8-d34c-4145-8f1a-0dd950a510b3'
 
 # see https://www.ibm.com/watson/developercloud/discovery/api/v1/?python#query-collection
 def query(qopts={}):
@@ -22,13 +22,13 @@ def add_from_url(url):
 		while True:
 			yield 'file{}.html'.format(counter)
 			counter += 1
-	
 	paper, file_iter = newspaper.build(url, memoize_articles = False), gen_filename()
+	
 	for article in paper.articles:
 		if('trump' in article.url.lower()):
 			article.download()
 			if article.html:
-				filename = next(file_gen)
+				filename = next(file_iter)
 				f = open(filename, 'w+')
 				f.write(article.html)
 				f.close()
@@ -38,4 +38,5 @@ def items(name, query):
 	return [i[name] for i in query]
 
 def add_document(filename):
-	discovery.add_document(environment_id, collection_id, '../{}'.format(filename))
+	with open((os.path.join(os.getcwd(), '', filename))) as fileinfo:
+		discovery.add_document(environment_id, collection_id, fileinfo = fileinfo)
