115d8deec97377cb79aa37fecc438b5a3188c204
dzdaniel84 <dzdaniel84@gmail.com>
2017-02-18 14:14:54 -0800

    Added some add paper methods

--- watson.py
+++ watson.py
@@ -1,3 +1,6 @@
+import sys
+import os
+import newspaper
 from watson_developer_cloud import DiscoveryV1
 
 discovery = DiscoveryV1(
@@ -13,4 +16,26 @@ collection_id = '33821e0a-5fef-4675-9f98-478b6f49cbe7'
 def query(qopts={}):
     return discovery.query(environment_id, collection_id, qopts)
 
-def add_document():
+def add_from_url(url):
+	def gen_filename():
+		counter = 0
+		while True:
+			yield 'file{}.html'.format(counter)
+			counter += 1
+	
+	paper, file_iter = newspaper.build(url, memoize_articles = False), gen_filename()
+	for article in paper.articles:
+		if('trump' in article.url.lower()):
+			article.download()
+			if article.html:
+				filename = next(file_gen)
+				f = open(filename, 'w+')
+				f.write(article.html)
+				f.close()
+				add_document(filename)
+
+def items(name, query):
+	return [i[name] for i in query]
+
+def add_document(filename):
+	discovery.add_document(environment_id, collection_id, '../{}'.format(filename))
