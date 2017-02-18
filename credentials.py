5d6e8534ad6a418dd37932bb712b658003a5266d
Max Ovsiankin <max@ovsankin.com>
2017-02-18 13:56:34 -0800

    Add watson

--- credentials.py
+++
@@ -1,7 +0,0 @@
-from watson_developer_cloud import DiscoveryV1
-
-discovery = DiscoveryV1(
-  username="9d4dc181-a636-46c3-9e7a-48a60a9b0255",
-  password="eIq0XfzlWdqI",
-  version="2016-12-01"
-)
---
+++ watson.py
@@ -0,0 +1,14 @@
+from watson_developer_cloud import DiscoveryV1
+
+discovery = DiscoveryV1(
+  username="9d4dc181-a636-46c3-9e7a-48a60a9b0255",
+  password="eIq0XfzlWdqI",
+  version="2016-12-01"
+)
+
+environment_id = 'f8c120ac-c67a-4983-ba71-693704418700'
+collection_id = '33821e0a-5fef-4675-9f98-478b6f49cbe7'
+
+# see https://www.ibm.com/watson/developercloud/discovery/api/v1/?python#query-collection
+def query(qopts):
+    return discovery.query(environment_id, collection_id, qopts)
