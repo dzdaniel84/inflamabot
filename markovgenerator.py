c99a0aeddd1c11dc881ae9bfaf7c8d04fe307cae
Max Ovsiankin <max@ovsankin.com>
2017-02-19 03:00:16 -0800

    Modify files

--- markovgenerator.py
+++ markovgenerator.py
@@ -1,26 +1,34 @@
 import markovify
-
-def gen_filename():
-		counter = 0
-		while True:
-			yield 'markov{}.txt'.format(counter)
-			counter += 1
+import random
+import math
 
 def get_text(loc):
     with open(loc) as f:
         text = f.read()
     return text.replace('\n', '')
 
-file_iter = gen_filename()
 
-def create_text(url, len = 100000):
-	filename = next(file_iter)
-	text_model = markovify.Text(get_text(url))
-	f = open(filename, 'w+')
-	for i in range(len):
-		try:
-			print('progress: {}'.format(i))
-			f.write(text_model.make_sentence())
-		except TypeError:
-			continue
-	f.close()
+def create_text(name, url, out, len=100000):
+    text_model = markovify.Text(get_text(url))
+    f = open(out, 'w+')
+    print('{} progress: 0%'.format(name), end='\r')
+    j = 0
+    for i in range(len):
+        try:
+            print('{} progress: {:6.3f}% [{}{}]'.format(name, i/len*100, '=' * j, ' ' * (10 - j)), end='\r')
+            j = round((1 + math.sin(i/100)) * 5)
+            f.write(text_model.make_sentence() + '\n')
+        except TypeError:
+            continue
+    f.close()
+
+people = ['bush', 'clinton', 'homer', 'jackson', 'kanye', 'kennedy', 'lincoln',
+          'obama', 'reagan', 'roosevelt', 'stalin', 'trump', 'washington']
+#people = ['trump']
+
+def loadpeople():
+    for person in people:
+        create_text(person, 'people/{}.txt'.format(person), 'out/{}.txt'.format(person))
+
+if __name__ == '__main__':
+    loadpeople()
